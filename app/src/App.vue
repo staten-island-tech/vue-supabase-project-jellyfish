07d7fa27b8144a265b4e73d70232eab1b1b6ca00
Stella-Long <huierl@nycstudents.net>
2026-05-20 12:17:19 -0400

    workinbg

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,22 +1,13 @@
+<template>
+  <div>
+<h2>test</h2>
+  </div>
+</template>
+
 <script setup>
-  import { ref, onMounted } from 'vue'
-  import { supabase } from './utils/supabase'
-  
-  const todos = ref([])
-
-  async function getTodos() {
-    const { data } = await supabase.from('todos').select()
-    todos.value = data
-  }
-
-  onMounted(() => {
-    getTodos()
-  })
 
 </script>
 
-<template>
-  <ul>
-    <li v-for="todo in todos" :key="todo.id">{{ todo.name }}</li>
-  </ul>
-</template>
+<style lang="scss" scoped>
+
+</style>
