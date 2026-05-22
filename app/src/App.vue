01bf65363d0bd5b1b213ee7645fdb103e37d6415
Elisej0072 <Elise22x08@gmail.com>
2026-05-22 12:09:05 -0400

    1

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,12 +1,7 @@
 <template>
-<<<<<<< HEAD
- <div>
-  <h1> </h1>
+
   <RouterView/>
- </div>
-=======
-  <router-view />
->>>>>>> dad15402d1e050614cff405aeb91c3a63554c4f2
+
 </template>
 
 <script setup></script>
