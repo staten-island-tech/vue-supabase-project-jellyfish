3a1fdd29031efbdf651a4bbafd191f8b64836827
Elisej0072 <Elise22x08@gmail.com>
2026-05-22 12:16:47 -0400

    1

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-<h1> Welcome {{ user.name }}! </h1>
+<h1> Welcome! </h1>
 <button @click="goToMovies(movie)"> Movies </button>
 <button @click="yourReviews(reviews)"> Your Reviews </button>
   <div class="container">
