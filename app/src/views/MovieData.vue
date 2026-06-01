ea632cee7dee7d255fe5328e67576cffbb0a05b5
Elisej0072 <Elise22x08@gmail.com>
2026-06-01 12:31:06 -0400

    1

--- app/src/views/MovieData.vue
+++ app/src/views/MovieData.vue
@@ -8,52 +8,9 @@
 <script setup>
  import {ref, onMounted} from 'vue'
  import MovieCard from '@/components/MovieCard.vue'
-<<<<<<< HEAD
-/*  async function getMovie(){
-=======
- const movie = ref([])
- async function getMovie(){
->>>>>>> 376e88dde60ea920c7be14eb5acf580d81a63ab5
-  try{
-    const response = await fetch('https://api.tvmaze.com/shows')
-    const data = await response.json()
-    movie.value = data.results
-  } catch(error) {
-    console.log(error)
-  }
- }
- onMounted(()=> {
-  getMovie()
- })
 
-<<<<<<< HEAD
-// async function fetchApiData() {
-//   const apiUrl = 'https://api.tvmaze.com/shows';
-=======
-/* async function fetchApiData() {
-  const apiUrl = 'https://api.tvmaze.com/shows';
->>>>>>> 376e88dde60ea920c7be14eb5acf580d81a63ab5
 
-//   try {
-    
-//     const response = await fetch(apiUrl);
-
-//     if (!response.ok) {
-//       throw new Error(`HTTP error! status: ${response.status}`);
-//     }
-    
-//     const data = await response.json();
-
-//     console.log(data);
-//     return data;
-
-//   } catch (error) {
-    
-//     console.error('Error fetching data:', error);
-//   }
-// }
-
-// fetchApiData();
+  fetchApiData();
    const route = useRoute() 
     const movie = ref(null)
     async function getMovie(id){
@@ -74,16 +31,6 @@
         }
     )
 
-<<<<<<< HEAD
-=======
-  } catch (error) {
-    
-    console.error('Error fetching data:', error);
-  }
-}
-
-fetchApiData(); */
->>>>>>> 376e88dde60ea920c7be14eb5acf580d81a63ab5
 
 </script>
 
