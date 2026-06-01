478d33f2933ad288b55350840fc60a40942a4752
Elisej0072 <Elise22x08@gmail.com>
2026-06-01 12:24:50 -0400

    1

--- app/src/components/MovieCard.vue
+++ app/src/components/MovieCard.vue
@@ -1,10 +1,10 @@
 <template>
-    <div>
+<router-link :to='Path' class="card"><div class="container"></div>
 <h2> {{ movie.name }} </h2>
 <h3> {{ movie.rating }} </h3>
 <h3> {{ movie.summary }} </h3>
 <button @click="reviewMovie(movie)"> Add a Review </button>
-    </div>
+</router-link>
 </template>
 
 <script setup>
--- app/src/views/MovieData.vue
+++ app/src/views/MovieData.vue
@@ -8,7 +8,6 @@
 <script setup>
  import {ref, onMounted} from 'vue'
  import MovieCard from '@/components/MovieCard.vue'
- const movie = ref([])
 /*  async function getMovie(){
   try{
     const response = await fetch('https://api.tvmaze.com/shows')
@@ -22,29 +21,49 @@
   getMovie()
  }) */
 
-async function fetchApiData() {
-  const apiUrl = 'https://api.tvmaze.com/shows';
+// async function fetchApiData() {
+//   const apiUrl = 'https://api.tvmaze.com/shows';
 
-  try {
+//   try {
     
-    const response = await fetch(apiUrl);
+//     const response = await fetch(apiUrl);
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! status: ${response.status}`);
+//     }
+    
+//     const data = await response.json();
+
+//     console.log(data);
+//     return data;
+
+//   } catch (error) {
+    
+//     console.error('Error fetching data:', error);
+//   }
+// }
+
+// fetchApiData();
+   const route = useRoute() 
+    const movie = ref(null)
+    async function getMovie(id){
+        const response = await fetch(`https://api.tvmaze.com/shows/${id}`)
+        const data = await response.json()
+        movie.value = data
+        console.log(data)
     }
     
-    const data = await response.json();
+    onBeforeMount(function(){
+        getMovie(route.params.id)
+    })
 
-    console.log(data);
-    return data;
+    watch(
+        () => route.params.id,
+        function(id) {
+            getAnimal(id)
+        }
+    )
 
-  } catch (error) {
-    
-    console.error('Error fetching data:', error);
-  }
-}
-
-fetchApiData();
 
 </script>
 
