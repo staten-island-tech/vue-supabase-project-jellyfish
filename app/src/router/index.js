a389bc51522ae480e5ff29b70b672224c2286b09
alexavso <alexasolanou@gmail.com>
2026-06-02 12:11:41 -0400

    .

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import UserCreate from '@/views/UserCreate.vue'
-import MovieData from '@/views/ShowData.vue'
+import ShowData from '@/views/ShowData.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
