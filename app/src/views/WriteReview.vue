7c2f851e49f06238e5b0aa44023f1dfeae996c33
Elisej0072 <Elise22x08@gmail.com>
2026-06-05 12:07:31 -0400

    1

---
+++ app/src/views/WriteReview.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+  import {ref} from 'vue'
+  let id = 0
+
+const writeReview = ref('')
+const reviews = ref([
+  { id: id++, text: 'show is very good', post: true },
+])
+
+function writeReview() {
+  reviews.value.push({ id: id++, text: writeReview.value, post: false })
+  writeReview.value = ''
+}
+
+function removeReview(review) {
+  reviews.value = reviews.value.filter((r) => r !== review)
+}
+
+<form action="submit" @submit.prevent="writeReview">
+    
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
