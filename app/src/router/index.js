9de39c34e09cd3e712670aaa56c66d89d494d705
alexavso <alexasolanou@gmail.com>
2026-06-12 12:04:01 -0400

    .

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import UserCreate from '@/views/UserCreate.vue'
 import ShowData from '@/views/ShowData.vue'
-import WriteReview from '@/views/WriteReview.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,11 +20,6 @@ const router = createRouter({
      path: '/shows/:id',
      name: 'shows',
      component: ShowData,
-    },
-     {
-     path: '/reviews',
-     name: 'reviews',
-     component: WriteReview,
     },
   ],
 })
